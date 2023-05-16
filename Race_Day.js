36044710223735858d3d0d9a28e2c2de01966dad
Lokeshsri11 <lokeshsri1109@gmail.com>
2023-05-16 17:04:50 +0530

    race day project

---
+++ Race_Day.js
@@ -0,0 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registerdEarly = true;
+const runnerAge = 20;
+if (runnerAge > 18 && registerdEarly) {
+    raceNumber += 10000;
+}
+
+if (runnerAge > 18 && registerdEarly) {
+    console.log(`You will race at 9:30 am and your race number is ${raceNumber}`);
+} else if (runnerAge > 18 && !registerdEarly) {
+    console.log(`you will race at 11:00 am and your race number is ${raceNumber}`)
+} else if (runnerAge < 18 && !registerdEarly) {
+    console.log(`you will race at 12:30 am and your race number is ${raceNumber}`)
+} else if (runnerAge === 18) {
+    console.log('Please see registration desk');
+}
